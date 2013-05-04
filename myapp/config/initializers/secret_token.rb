9dd54a8aea8e6e87030a6fe554b2910ef6c8d4c7
Kazuho Yamaguchi <kyamaguchi@mediweb.jp>
2013-05-04 12:41:26 +0900

    Update sandbox

--- myapp/config/initializers/secret_token.rb
+++ myapp/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = '2b922b5f047fbea1e2c56e6f155dd9e26334ebf8f623dce7967528e2640f8abd77b189903eb91acff749fca870dce53aff19b7153a7d7b7047f75a0bb48130be'
+Myapp::Application.config.secret_token = '8f64209639fe132d6c4ba5bbe8a7aa7ef245bea0af2e466f935a50059026c00acd267615d031a43b4109abaf3372047ba242f5e85bfb0fe7967dd4b9f2f26e24'
